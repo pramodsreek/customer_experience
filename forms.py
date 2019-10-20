a24f33a07f1007d926aaf76b917f79baf0dcfdd3
Pramod S <pramodsree@yahoo.com>
2019-10-20 23:08:20 +1100

    reformat

--- forms.py
+++ forms.py
@@ -2,13 +2,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, validators
 from wtforms.validators import DataRequired, Length
 
+
 class TwitterHandleForm(FlaskForm):
-    '''
+    """
     A class to handle fields and validation in a flask form.
-    '''
-    twitter_handle = StringField('Enter your Twitter Handle', validators = [DataRequired(), Length(min=5, max=10)])
-    competitors_twitter_handle = StringField('Enter your Competitors Twitter Handle', validators = [DataRequired(), Length(min=5, max=10)])
+    """
+
+    twitter_handle = StringField(
+        "Enter your Twitter Handle", validators=[DataRequired(), Length(min=5, max=10)]
+    )
+    competitors_twitter_handle = StringField(
+        "Enter your Competitors Twitter Handle",
+        validators=[DataRequired(), Length(min=5, max=10)],
+    )
     twitter_handle_error = StringField("")
     twitter_handle_error.data = " "
 
-    submit = SubmitField('Sentiment Comparison with Competitor & Customer Support Data')
+    submit = SubmitField("Sentiment Comparison with Competitor & Customer Support Data")
+
